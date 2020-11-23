e4eddd16619f086a21625f2f89af411ff78d41ee
Matheus Tadeu <mtqr1@hotmail.com>
2020-11-22 23:28:22 -0300

    Add Fix Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,13 @@
 import React from "react";
 
 import Typography from "@material-ui/core/Typography";
-import Link from "@material-ui/core/Link";
 
 export default function Footer() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
+        Loja Surf Art Brasil
+        {" "}
       {new Date().getFullYear()}
       {"."}
     </Typography>
